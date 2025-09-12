88cb182682ae6c035949d7e814f11206a32a9e3c
258056 <258056_evn@entetsu.co.jp>
2025-09-12 16:18:44 +0700

    update project

--- src/models/user.ts
+++ src/models/user.ts
@@ -17,7 +17,7 @@ class User extends Model<UserAttributes, UserCreationAttributes>
     public name!: string;
     public email!: string;
     public password!: string;
-    public role!: string;
+    public role!:     string;
 }
 
 User.init(
